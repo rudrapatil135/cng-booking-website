466438d2cd6dc696408e957d8b67040e19814e8e
Rudra <Rudrapatil1355@gmail.com>
2025-11-28 02:15:07 +0530

    Fix ALLOWED_HOSTS for Render

--- cng/settings.py
+++ cng/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Use Whitenoise to serve static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
