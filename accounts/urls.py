eca19a2fd1757b24ca20bbb2263975b776952812
Rudra <Rudrapatil1355@gmail.com>
2025-11-28 23:36:39 +0530

    Fix SECRET_KEY and deployment config

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('aa', admin.site.urls),
-    path('home/', views.home,name = 'home'),
-    path('',views.indexpage, name = 'indexpage'),
+    path('index/', views.indexpage,name = 'indexpage'),
+    path('',views.home, name = 'home'),
     path('registration/',views.registration,name='registration'),
     path('login/',views.user_login,name = 'login'),
     path('logout/',views.user_logout,name='logout'),
