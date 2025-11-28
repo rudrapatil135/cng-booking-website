626a44b2b8d7999dc2975a5b363c2581209f7054
Rudra <Rudrapatil1355@gmail.com>
2025-11-28 23:43:28 +0530

    Fix SECRET_KEY and deployment config

--- cng/settings.py
+++ cng/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
